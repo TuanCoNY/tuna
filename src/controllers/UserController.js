e6f7a16a3298d2a7efaa85d27aa5ee7035ad36b7
Your Name <you@example.com>
2024-11-18 14:56:35 +0700

    fix_createrOrder

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -62,7 +62,7 @@ const loginUser = async (req, res) => {
             samesite: 'strict',
             path: '/'
         })
-        return res.status(200).json(newResponse)
+        return res.status(200).json({ ...newResponse, refresh_token })
     } catch (e) {
         return res.status(404).json({
             message: e,
@@ -157,7 +157,8 @@ const getDetailsUser = async (req, res) => {
 
 const refreshToken = async (req, res) => {
     try {
-        const token = req.cookies.refresh_token
+        // const token = req.cookies.refresh_token
+        const token = req.headers.token.split(' ')[1]
         if (!token) {
             return res.status(200).json({
                 status: 'ERR',
--- src/routes/OrderRouter.js
+++ src/routes/OrderRouter.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const OrderController = require('../controllers/OrderController');
 const { authUserMiddleware, authMiddleware } = require("../middleware/authMiddleware");
 
-router.post('/create', authUserMiddleware, OrderController.createOrder)
+router.post('/create', OrderController.createOrder)
 router.get('/get-all-order/:id', authUserMiddleware, OrderController.getAllOrderDetails)
 router.get('/get-details-order/:id', authUserMiddleware, OrderController.getDetailsOrder)
 router.delete('/cancel-order/:id', authUserMiddleware, OrderController.cancelOrderDetails)
